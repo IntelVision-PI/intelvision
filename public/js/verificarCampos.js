96a113aabd337789fd55691cc10976d9eaf0c49f
Bruno Oliveira <bruno.doliveira@sptech.school>
2025-08-31 19:48:44 -0300

    feat: integrando criacao de usuarios com o backend

--- public/js/verificarCampos.js
+++ public/js/verificarCampos.js
@@ -1,8 +1,8 @@
 function verifyFields(fields){
     let valid = true;
     for(field of fields){
-        if(field.value == '' || (field.getAttribute('id') == 'email' && !field.value.includes("@"))){
-            field.style.borderColor = 'rgba(255, 0, 0, 0.4)';
+        if(field.value == '' || (field.getAttribute('type') == 'email' && !field.value.includes("@"))){
+            field.style.borderColor = 'rgba(255, 0, 0, 1)';
             valid = false;
         }else{
             field.style.borderColor = '#00863F';
