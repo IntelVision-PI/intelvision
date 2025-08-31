8e12fbfebe6e82b2038fd1cd8bd3acb4de364019
Bruno Oliveira <bruno.doliveira@sptech.school>
2025-08-31 19:02:30 -0300

    feat: integrando alteração de nome com o backend

--- public/js/edit_name.js
+++ public/js/edit_name.js
@@ -3,6 +3,7 @@ const out_edit_name_local = document.getElementById('out_edit_name');
 const close_edit_name_button_local = document.getElementById('close_edit_name_button');
 const edit_name_modal_local = document.getElementById('edit_name_modal');
 const cancel_button_edit_local_name = document.getElementById('cancel_button_edit_name');
+const submit_button_edit_name = document.getElementById('submit_button_edit_name');
 
 const close_modal_name = () => {
     out_edit_name_local.style.visibility = 'hidden';
@@ -22,8 +23,29 @@ const open_modal_name = () => {
     edit_name_modal_local.style.opacity = 1;
 }
 
+const sendNameEmpresa = () => {
+    const nome = document.querySelector('.edit_name_content .edit_name_field input').value;
+
+    fetch('empresas/atualizar/empresa/nome', {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "nome": nome,
+            "idEmpresa": sessionStorage.getItem('id')
+        }),
+    }).then(response => {
+        if (response.status == 200) {
+            sessionStorage.setItem('nome', nome);
+            window.location.reload();
+        }
+    })
+}
+
 edit_name_local.addEventListener('click', open_modal_name)
 
 out_edit_name_local.addEventListener('click', close_modal_name)
 close_edit_name_button_local.addEventListener('click', close_modal_name)
 cancel_button_edit_local_name.addEventListener('click', close_modal_name)
+submit_button_edit_name.addEventListener('click', sendNameEmpresa)
