dd813c88446ee2bb64384f938558aeda47be7a23
Bruno Oliveira <bruno.doliveira@sptech.school>
2025-08-31 19:06:39 -0300

    feat: integrando alteração de email com o backend

--- public/js/edit_email.js
+++ public/js/edit_email.js
@@ -3,6 +3,7 @@ const out_edit_email_local = document.getElementById('out_edit_email');
 const close_edit_email_button_local = document.getElementById('close_edit_email_button');
 const edit_email_modal_local = document.getElementById('edit_email_modal');
 const cancel_button_edit_local = document.getElementById('cancel_button_edit');
+const submit_button_edit_email = document.getElementById('submit_button_edit_email');
 
 const close_modal = () => {
     out_edit_email_local.style.visibility = 'hidden';
@@ -22,8 +23,29 @@ const open_modal = () => {
     edit_email_modal_local.style.opacity = 1;
 }
 
+const sendEmailEmpresa = () => {
+    const email = document.querySelector('.edit_email_content .edit_email_field input').value;
+
+    fetch('empresas/atualizar/empresa/email', {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "email": email,
+            "idEmpresa": sessionStorage.getItem('id')
+        }),
+    }).then(response => {
+        if (response.status == 200) {
+            sessionStorage.setItem('email', email);
+            window.location.reload();
+        }
+    })
+}
+
 edit_email_local.addEventListener('click', open_modal)
 
 out_edit_email_local.addEventListener('click', close_modal)
 close_edit_email_button_local.addEventListener('click', close_modal)
 cancel_button_edit_local.addEventListener('click', close_modal)
+submit_button_edit_email.addEventListener('click', sendEmailEmpresa)
