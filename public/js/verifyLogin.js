300e03715dfe7e7d9e9cc701054309bcc38537f8
Bruno Oliveira <bruno.doliveira@sptech.school>
2025-09-01 21:17:41 -0300

    feat: verifica se usuario está logado para trocar o icone

---
+++ public/js/verifyLogin.js
@@ -0,0 +1,20 @@
+const verificarSeUsuarioEstaLogado = () => {
+    const localButtons = document.querySelector('nav .buttons_account');
+    if(sessionStorage.getItem('type') == 'empresa'){
+        localButtons.innerHTML = `
+            <li>
+                <div class="nav_photo">
+                    <a href="empresa-profile.html"><img src="assets/profile/default_avatar_empresa.jpeg"></a>
+                </div>
+            </li>
+        `;
+    }else if(sessionStorage.getItem('type') == 'usuario'){
+        localButtons.innerHTML = `
+            <li>
+                <div class="nav_photo">
+                    <a href="user-profile"><img src="assets/profile/default_avatar.webp"></a>
+                </div>
+            </li>
+        `;
+    }
+}
