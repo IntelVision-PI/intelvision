2e08a6aa7799e43f4aae9bb3d9d302a3393c02b5
LucasTsuzukuSPTECH <lucas.tsuzuku@sptech.school>
2025-10-07 21:48:49 -0300

    feat: alteracoes scripts bd

--- src/database/script_intelvision.sql
+++ src/database/script_intelvision.sql
@@ -1,12 +1,7 @@
 -- drop database intelvision;
 create database intelvision;
 use intelvision;
-create table endereco(
-	id int not null primary key auto_increment,
-    cep char(9) not null,
-    numero int not null,
-    complemento varchar(100)
-);
+-- Script para criação do banco de dados Intelvision
 
 create table empresa(
 	id int not null primary key auto_increment,
@@ -16,9 +11,7 @@ create table empresa(
     senha varchar(100) not null,
     codigo_ativacao varchar(45) not null,
     telefone varchar(20),
-    razao_social varchar(100),
-    fkEndereco int not null,
-    foreign key (fkEndereco) references endereco(id)
+    razao_social varchar(100)
 );
 
 create table usuario(
@@ -29,19 +22,3 @@ create table usuario(
     fkEmpresa int not null,
     foreign key (fkEmpresa) references empresa(id)
 );
-
-show tables;
-desc endereco;
-desc empresa;
-desc usuario;
-
-insert into endereco (cep, numero, complemento) 
-values 
-('12345-678', 100, null);
-insert into empresa (cnpj, nome, email, senha, codigo_ativacao, telefone, razao_social, fkEndereco) 
-values
-('123456789', 'Intelbras', 'intelbras@intelbras.com', '123456789','ATV123', '(11) 95289-8922','INTELBRAS S.A. INDUSTRIA DE TELECOMUNICACAO ELETRONICA BRASILEIRA', 1);
-
-
-select * from usuario;
-select id from empresa where codigo_ativacao='ATV123';
