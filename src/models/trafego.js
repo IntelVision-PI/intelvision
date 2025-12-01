17968e5e9147c22f110ee46d7f7f4b7c0dfa6b5a
ViniRocha1 <vinicius.barros@sptech.school>
2025-12-01 11:28:26 -0300

    fix: limitando gráfico de ultimos alertas /trafego.html

--- src/models/trafego.js
+++ src/models/trafego.js
@@ -1,12 +1,10 @@
 var database = require("../database/config");
 const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
 
-// Configuração do cliente S3 (Pega as credenciais do "aws configure" ou variáveis de ambiente)
 const s3 = new S3Client({ 
     region: process.env.AWS_REGION || "us-east-1" 
 });
 
-// Função auxiliar para transformar o stream do S3 em string/json
 async function streamParaString(stream) {
     return await new Promise((resolve, reject) => {
         let data = "";
@@ -16,12 +14,10 @@ async function streamParaString(stream) {
     });
 }
 
-// Busca o JSON no Bucket S3
 async function buscarDadosS3(ano, mes, dia, servidor) {
-    const bucket = "my-bucket-client-nicolas"; // Seu bucket
+    const bucket = "my-bucket-client-nicolas";
     const servidorTransformado = servidor.toLowerCase();
     
-    // Caminho do arquivo
     const key = `${ano}/${mes}/${dia}/dados_maquina_${ano}-${mes}-${dia}--${servidorTransformado}.json`;
     
     console.log(`[Model] Buscando no S3: ${key}`);
@@ -39,7 +35,6 @@ async function buscarDadosS3(ano, mes, dia, servidor) {
     }
 }
 
-// Busca a lista de servidores no Banco de Dados (MySQL/SQL Server)
 function buscarServidores() {
     var instrucao = `
         SELECT * FROM servidor;
