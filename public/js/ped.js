8644742a197c818fbd322163a079fc2402f3c2d6
Gabriel1Ribeiro <gabriel.hribeiro@sptech.school>
2025-12-01 11:22:45 -0300

    Integrando o teste com os dados

---
+++ public/js/ped.js
@@ -0,0 +1,10 @@
+const servidores = [
+    { nome: "Servidor A", riscoConfig: 70, ultimos4: [40, 50, 55, 60] },
+    { nome: "Servidor B", riscoConfig: 80, ultimos4: [65, 70, 72, 75] },
+    { nome: "Servidor C", riscoConfig: 75, ultimos4: [50, 55, 60, 65] },
+    { nome: "Servidor D", riscoConfig: 85, ultimos4: [75, 78, 82, 88] },
+    { nome: "Servidor E", riscoConfig: 70, ultimos4: [30, 38, 44, 50] },
+    { nome: "Servidor C", riscoConfig: 75, ultimos4: [50, 55, 60, 65] },
+    { nome: "Servidor D", riscoConfig: 85, ultimos4: [75, 78, 82, 88] },
+    { nome: "Servidor E", riscoConfig: 70, ultimos4: [30, 38, 44, 50] }
+  ];
