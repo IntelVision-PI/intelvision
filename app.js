a354ec334277aaf338053f6d26b13dff8fcfea9f
LucasTsuzukuSPTECH <lucas.tsuzuku@sptech.school>
2025-12-01 00:30:48 -0300

    descomentando app.js para usar a rota de dados

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // var ambiente_processo = 'producao';
-var ambiente_processo = 'desenvolvimento';
+var ambiente_processo = "desenvolvimento";
 
-var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
+var caminho_env = ambiente_processo === "producao" ? ".env" : ".env.dev";
 // Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
 // A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
 
@@ -18,18 +18,15 @@ var app = express();
 var usuarioRouter = require("./src/routes/usuarios");
 var empresaRouter = require("./src/routes/empresas");
 var servidorRouter = require("./src/routes/servidores");
-// var dadosRouter = require("./src/routes/comparativo");
+var dadosRouter = require("./src/routes/comparativo");
 
-// const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
-
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
 
 app.use(cors());
 
-// const s3 = new S3Client({
-//   region: "us-east-1"
-// });
-
-
+const s3 = new S3Client({
+  region: "us-east-1",
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -40,12 +37,10 @@ app.use(cors());
 app.use("/usuarios", usuarioRouter);
 app.use("/empresas", empresaRouter);
 app.use("/servidores", servidorRouter);
-// app.use("/dados", dadosRouter);
-
-
+app.use("/dados", dadosRouter);
 
 app.listen(PORTA_APP, function () {
-    console.log(`
+  console.log(`
     ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
     ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
     ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
