fcf72477d906f128e99768198ff925942104105a
leticia0510 <leticia.ssantos@sptech.school>
2025-12-01 03:09:13 -0300

    puxando os dados do s3

--- src/controllers/situacaoController.js
+++ src/controllers/situacaoController.js
@@ -1,18 +1,24 @@
 var situacaoModel = require("../models/situacaoModel");
 
-async function buscarDadosS3(req, res) {
-    const { ano, mes, dia, servidor } = req.params;
-    const resultado = await comparativoModel.buscarDadosS3(ano, mes, dia, servidor);
-    if (!resultado) {
-        return res.status(404).json({ erro: "Arquivo não encontrado no S3" });
-    }
-    res.json(resultado);
+function buscarParametro(req, res) {
+    let idComponente = req.body.idComponente;
+
+    situacaoModel.buscarParametro(idComponente).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar Paramêtros por empresa.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    })
 }
 
-function buscarParametros(req, res) {
-    var idEmpresa = req.params.idEmpresa;
+function buscarTodos(req, res) {
 
-    situacaoModel.buscarParametros(idEmpresa).then(function(resultado){
+    situacaoModel.buscarTodos().then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -26,6 +32,6 @@ function buscarParametros(req, res) {
 }
 
 module.exports = {
-    buscarDadosS3,
-    buscarParametros
+    buscarParametro,
+    buscarTodos
 };
