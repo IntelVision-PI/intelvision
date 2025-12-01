d3575cde2ad5cc684ec22686cddd4c7b80e94bae
Gabriel1Ribeiro <gabriel.hribeiro@sptech.school>
2025-12-01 07:42:10 -0300

    Tirando o .json

--- public/js/predicao.js
+++ public/js/predicao.js
@@ -8,12 +8,12 @@ async function carregarArquivosS3() {
     const dia = "27";
 
     // Arquivo Ano Atual
-    const r2025 = await fetch(`/predicoes/${ano}/${mes}/${dia}/servidor14.jasudjc-2025.json`);
+    const r2025 = await fetch(`/predicoes/${ano}/${mes}/${dia}/servidor14.jasudjc-2025`);
     if (r2025.ok) dados2025 = await r2025.json();
     else console.error("Erro ao buscar servidor14.jasudjc-2025.json");
 
     // Arquivo Ano Anterior
-    const r2024 = await fetch(`/predicoes/${ano}/${mes}/${dia}/servidor14.jasudjc-2024.json`);
+    const r2024 = await fetch(`/predicoes/${ano}/${mes}/${dia}/servidor14.jasudjc-2024`);
     if (r2024.ok) dados2024 = await r2024.json();
     else console.error("Erro ao buscar servidor14.jasudjc-2024.json");
 
