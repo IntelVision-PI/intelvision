85050dec03ac629090886e35d46fc8125c3c64ed
Gabriel1Ribeiro <gabriel.hribeiro@sptech.school>
2025-12-02 14:50:14 -0300

    alterando o fetch do s3 de predição

--- public/js/predicao.js
+++ public/js/predicao.js
@@ -8,13 +8,13 @@ async function carregarArquivosS3() {
     const dia = "27";
 
     // Arquivo Ano Atual
-    const r2025 = await fetch(`/predicoes/${ano}/${mes}/${dia}/servidor14.jasudjc-2025.json`);
+    const r2025 = await fetch(`/predicoes/${ano}/${mes}/${dia}/${servidor}`);
 
     if (r2025.ok) dados2025 = await r2025.json();
     else console.error("Erro ao buscar servidor14.jasudjc-2025.json");
 
     // Arquivo Ano Anterior
-    const r2024 = await fetch(`/predicoes/${ano}/${mes}/${dia}/servidor14.jasudjc-2024.json`);
+    const r2024 = await fetch(`/predicoes/${ano}/${mes}/${dia}/${servidor}`);
     if (r2024.ok) dados2024 = await r2024.json();
     else console.error("Erro ao buscar servidor14.jasudjc-2024.json");
 
@@ -24,12 +24,12 @@ async function carregarArquivosS3() {
 }
 
 async function pegarDadosS3(ano, mes, dia, servidor) {
-            const url = `/dados/${ano}/${mes}/${dia}/${servidor}`;
-            try {
-                const res = await fetch(url, { cache: "no-store" });
-                if (!res.ok) return [];
-                const json = await res.json();
-                return json.dados || json || [];
-            } catch { return []; }
-        }
+  const url = `/dados/${ano}/${mes}/${dia}/${servidor}`;
+  try {
+      const res = await fetch(url, { cache: "no-store" });
+      if (!res.ok) return [];
+      const json = await res.json();
+      return json.dados || json || [];
+  } catch { return []; }
+}
 
